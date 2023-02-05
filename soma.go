7fc638ce4db1e0eb17eb17fb31229c8e6793d0d9
Roberto Macedo <macedorss20@gmail.com>
2023-02-05 14:14:14 -0300

    Exercício com funções

---
+++ soma.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+func hello(nome string){      //Função com argumento nome 
+	fmt.Println("Olá", nome)
+}
+
+func soma(a, b int) int{   //Função soma
+	return a + b
+}
+
+func conv(a int, b string) int{  // Função conver o argumento "b" no caso string para inteiro
+	i, _ := strconv.Atoi(b)   	 // convete e soma os valores	
+	return a + i
+}
+
+func main(){
+	hello("Roberto Macedo")
+	fmt.Println("Total:", soma(25, 4))
+	fmt.Println("Total:", conv(25, "10")) //Saídas do programa
+}
