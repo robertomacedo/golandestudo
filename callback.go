b165fbff36df4e2e9c095c3d874c06671c1617ed
Roberto Macedo <macedorss20@gmail.com>
2023-02-10 10:28:43 -0300

    Exercício com funções exemplo de callback

---
+++ callback.go
@@ -0,0 +1,28 @@
+package main 
+
+import "fmt"
+
+func main(){
+
+	t := somentePares(soma, []int{50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60}...)
+	fmt.Println("Soma de os números impares",t)
+	//fmt.Println("Soma de os números Pares",t)
+}
+func soma(x ...int) int{
+	n := 0
+	for _, v := range x{
+		n += v
+	}
+	return n 
+}
+func somentePares(f func(x ...int) int, y ...int) int{
+	var slice []int 
+	for _, v := range y {
+		if v%2 != 0 {
+			slice = append(slice, v)
+		}
+		
+	}
+	total := f(slice...)
+	return total 
+}
