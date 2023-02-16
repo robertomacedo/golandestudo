0372c9b1585f57e0d617cb732ca84e7c18979909
Roberto Macedo <macedorss20@gmail.com>
2023-02-16 09:51:35 -0300

    exercício usando canal

---
+++ canalgo.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+	"sync"
+)
+
+func main() {
+	canal1 := make(chan int)
+	canal2 := make(chan int)
+	funções := 5
+	
+	go manda(100, canal1)
+	go outra(funções, canal1, canal2)
+
+	for v  := range canal2 {
+		fmt.Println(v)
+	}
+}
+
+func manda(n int, canal chan int){
+	for i := 0; i < n; i++{
+		canal <- i 
+	}
+	close(canal)
+
+}
+
+func outra(funções int, canal1, canal2 chan int){
+	var wg sync.WaitGroup
+	
+	for i := 0; i < funções; i++ {
+		wg.Add(1)
+		go func(){
+			for v := range canal1 {
+				canal2 <- trabalho(v)
+			}
+			wg.Done()
+			}()
+		}
+		wg.Wait()
+		close(canal2)
+	}
+func trabalho(n int) int{
+	time.Sleep(time.Millisecond * time.Duration(rand.Intn(1e3)))
+	return n
+}
