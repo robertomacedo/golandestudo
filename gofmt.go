ec7524f23701d5c64c999e7212bd0991a0526d6e
Roberto Macedo <macedorss20@gmail.com>
2023-03-07 19:45:37 -0300

    treinando o uso de var em G

---
+++ gofmt.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+
+
+var goConferenceName = "Go Conferência"
+var name = "João das Couves"
+var p = fmt.Println 
+
+
+func main() {
+
+	p("Olá! vamos à", goConferenceName, "na sexta o palestrante será o", name)
+	
+}
